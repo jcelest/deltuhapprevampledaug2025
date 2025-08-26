798b3a042c3fdab0ffbba309c899f96834ad0ad4
jcelest <joshuacelestin@Gmail.com>
2025-08-26 02:49:41 -0400

    pricingmatrixupdate6

--- new-option-calculator/frontend/src/lib/components/PricingMatrix.svelte
+++ new-option-calculator/frontend/src/lib/components/PricingMatrix.svelte
@@ -996,7 +996,7 @@
     }
 
     .stock-price-cell .price-value {
-      font-size: 0.5625rem;
+      font-size: 0.4rem;
       line-height: 1;
       text-align: center;
       white-space: nowrap;
@@ -1145,7 +1145,7 @@
     }
 
     .stock-price-cell .price-value {
-      font-size: 0.5rem;
+      font-size: 0.4rem;
       line-height: 1;
       text-align: center;
       white-space: nowrap;
